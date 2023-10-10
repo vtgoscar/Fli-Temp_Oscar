f2264d9a611e64c39f79b55e5d4cdfbf3cd299ba
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-10-10 02:30:40 +0000

    First commit

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar class="bg-white">
+			<svelte:fragment slot="lead">
+				<a href="/">
+					<img src="/FLI_BLK.png" alt="FLI GOLF Logo" class="logo-class" />
+				</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-lg variant-ghost-surface"
+					href="mailto:media@fligolf.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Media Email
+				</a>
+				<a
+					class="btn btn-lg variant-ghost-surface"
+					href="mailto:info@fligolf.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Info Email
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
